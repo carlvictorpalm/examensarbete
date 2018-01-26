8de5a82455845db97646996e1c1eb2c8cba2fb10
kimdahlberg <dahlberg.kim@hotmail.com>
2018-01-26 10:47:51 +0100

    aboutus text uppdaterad css, fix hover button på contact form

--- wp-content/themes/efterlangtad/about.php
+++ wp-content/themes/efterlangtad/about.php
@@ -10,36 +10,37 @@ get_header(); ?>
 
             <div class="aboutusimg col-md-12">
                 <img src=" <?php echo get_bloginfo('template_url') ?>/img/about_us_img.jpg" alt="">
-
-            </div>        </div>
+            </div>
+        </div>
     </div>
 </div> <!--Wrapper ends-->
 
-
     <div class="wrapper">
-        <div class="col-lg-8 ingress">
+        <div class="aboutuspage col-lg-8 ingress">
             <h3>Hey!</h3>
-            <h4>
-                 Vi är Victor Palm och Kim Dahlberg,
-                Två klasskompisar som beslutade sig för att göra sitt examensarbet tillsammans, det kom till att bli  — efterlängtad.se.
-                Victor och Kim studerar sista terminen på en webbutvecklingsutbildning med fokus på e-handel, trendigt, fräscht och framtiden!
-            </h4>
-            <h4>bakom den utvecklingen ligger en oroande trend och växa fram genom lätt tillgängligheten att skapa digitala försäljningsmöjligheter.
-                att skapa fler e-handels företag även bidra till en massproduktion med vår planet som insats.
-                Vi ville istället se på en e-handel med en produkt framtagen på minimal påverkan av jorden resuser som dessutom kunde skapas efter behov.</h4>
+            <h4>Vi är Victor Palm och Kim Dahlberg,
+                Två klasskompisar som beslutade sig för att göra sitt examensarbet tillsammans.
+                Victor och Kim studerar sista terminen på en webbutvecklingsutbildning med fokus på e-handel.
+                <i>"Trendigt, fräscht och framtiden"</i> - Säger omvärlden.</h4>
 
+            <h4>Men bakom alla filter och tillgänglighet av handel alla timmar på dygnet växer en farlig trend sig allt starkare.
+                Genom att öppna upp för fri handel digitalt bidrar vi också med en förstärkt massproduktion och ett merbehov som vi
+                annars inte skulle identifiera oss med och allt på bekostnad av jordens resuser.</h4>
 
+            <h4>Vi ville i stället utveckla en e-handel med en produkt framtagen på minimal påverkan av jordens resuser,
+                som dessutom produceras när konsumenten gör sin beställning, där hundratals artiklar inte existerar för
+                merköp och där vi kan stå för vad vi säljer. Då har vi i alla fall gjort det vi kunnat för att minimera
+                skövlingen av vår planets resuser och det tycker vi är ett bra steg i e-handelsutvecklingen. Det kom till att bli -</h4>
+            <h5>efterlängtad.se</h5>
         </div>
+
         <div class="col-lg-4 workspace">
             <div class="process">
                 <a href="https://drive.google.com/drive/folders/1OIJNDsnOYmhjXTurSEFdPo97LRlAP8vF" target="_blank"><img src=" <?php echo get_bloginfo('template_url') ?>/img/about_img_projekt.jpg" alt=""></a>
-
-
             </div>
             <div class="merit">
                 <a href="http://localhost/examensarbete/kontakt/" target="_blank"><img src=" <?php echo get_bloginfo('template_url') ?>/img/about_img_kontakt.jpg" alt=""></a>
             </div>
-
         </div>
 
 
