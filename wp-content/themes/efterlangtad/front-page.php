59179f921a1e9f10facc392400b98a870b1d3a15
kimdahlberg <dahlberg.kim@hotmail.com>
2018-01-26 11:34:15 +0100

    länkning av bilder

--- wp-content/themes/efterlangtad/front-page.php
+++ wp-content/themes/efterlangtad/front-page.php
@@ -106,19 +106,23 @@
     <div class="sale col-lg-12">
         <div class="col-lg-6">
             <div class="salediscount">
+                <a href="http://localhost/examensarbete/produkt/hasse/">
                 <img src="<?php echo get_bloginfo('template_url') ?>/img/rabatt.png" class="images" alt="">
                 <div class="middle">
                     <div class="text1">När du gör ditt första köp får du alltid 10% rabatt</div>
                 </div>
+                </a>
             </div>
         </div>
 
         <div class="col-lg-6">
             <div class="saleparents">
+                <a href="http://localhost/examensarbete/produkt/hasse/">
                 <img src="<?php echo get_bloginfo('template_url') ?>/img/parents.png" class="images" alt="">
                 <div class="middle">
                     <div class="text2">Ge bort en underbar gåva till nyblivna mormor, morfar, farmor, och farfar</div>
                 </div>
+                </a>
             </div>
         </div>
     </div>
@@ -127,7 +131,9 @@
 	  INSPIRATION
    -->
     <div class="inspo">
+        <a href="http://localhost/examensarbete/illustrationer/">
         <img src="<?php echo get_bloginfo('template_url') ?>/img/inspo.png" alt="">
+        </a>
     </div>
 
     <!-- 
