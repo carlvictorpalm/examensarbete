39541d2f5f53ff60dbb15d752d7a1db579ed7899
Victor Palm <carlvictorpalm@gmail.com>
2018-01-29 13:25:21 +0100

    början på validation för skapa tavla

--- wp-content/themes/efterlangtad/js/create-poster.js
+++ wp-content/themes/efterlangtad/js/create-poster.js
@@ -46,4 +46,42 @@
         });
 
     });
+
+        $("#productimage").validate(
+            {
+                rules:
+                    {
+                        birth_name:
+                            {
+                                required: true,
+                                maxlength: 10
+                            },
+                        birth_date:
+                            {
+                                required: true,
+                                maxlength:10
+                            },
+                        birth_length:
+                            {
+                                required: true,
+                                maxlength: 4
+                            },
+                        birth_weight:
+                            {
+                                required: true,
+                                maxlength:4
+                            },
+                        birth_hours:
+                            {
+                                required: true,
+                                maxlength:2
+                            },
+                        birth_minutes:
+                            {
+                                required: true,
+                                maxlength:2
+                            }
+                    }
+
+    });
 })(jQuery);
