c991e3ebdbd7628261c91863c90ed9df7a5d9578
Victor Palm <carlvictorpalm@gmail.com>
2018-01-30 09:05:37 +0100

    spaceing

--- wp-content/themes/efterlangtad/js/create-poster.js
+++ wp-content/themes/efterlangtad/js/create-poster.js
@@ -3,16 +3,13 @@
     $(document).ready(function () {
         //FÖDELSELÄNGD
         $(".birthlengthoutput").text('CM');
-
         //FÖDELSEVIKT
         $(".birthweightoutput").text(' GRAM');
-
         //FÖDELSETID
         $(".birthhours").text('00');
         $(".birthminutes").text('00');
         // FÖDELSEDAG
         $(".birthdayoutput").text('åååå-mm-dd');
-
         $("#birth_name").keyup(function(){
             var currentText = $(this).val();
             $(".birthnameoutput").text(currentText);
@@ -21,11 +18,7 @@
             var currentText = $(this).val();
             $(".birthdayoutput").text(currentText);
         });
-
         // INFO
-
-
-
         $("#birth_length").keyup(function(){
             var currentText = $(this).val();
             $(".birthlengthoutput").text(currentText + ' CM');
@@ -44,6 +37,5 @@
             var currentText = $(this).val();
             $(".birthminutes").text(currentText);
         });
-
     });
 })(jQuery);
