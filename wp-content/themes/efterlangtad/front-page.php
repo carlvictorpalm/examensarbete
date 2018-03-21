9110dcfe106fd0cb91169daa5f77194ebbf7d800
kimdahlberg <dahlberg.kim@hotmail.com>
2018-03-21 11:19:47 +0100

    update on front-page

--- wp-content/themes/efterlangtad/front-page.php
+++ wp-content/themes/efterlangtad/front-page.php
@@ -1,59 +1,59 @@
-<?php apply_filters('the_content',$wp_query->post->post_content);
+<?php apply_filters('the_content', $wp_query->post->post_content);
 ?>
-<!DOCTYPE html>
+    <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="<?php bloginfo('description'); ?>">
-    <title>
-		<?php bloginfo('name'); ?> |
-		<?php is_front_page() ? bloginfo('description') : wp_title(); ?>
-    </title>
-    <!-- Bootstrap core CSS -->
-    <link href="<?php bloginfo('template_url'); ?>/css/bootstrap.css" rel="stylesheet">
-    <!-- Custom styles for this template -->
-    <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
-	<?php wp_head(); ?>
-    <style>
-        .showcase{
-            background:url(<?php echo get_theme_mod('showcase_image', get_bloginfo('template_url').'/img/lapin.jpg'); ?>) no-repeat center center;
-        }
-    </style>
-</head>
+    <head>
+        <meta charset="<?php bloginfo('charset'); ?>">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <meta name="description" content="<?php bloginfo('description'); ?>">
+        <title>
+            <?php bloginfo('name'); ?> |
+            <?php is_front_page() ? bloginfo('description') : wp_title(); ?>
+        </title>
+        <!-- Bootstrap core CSS -->
+        <link href="<?php bloginfo('template_url'); ?>/css/bootstrap.css" rel="stylesheet">
+        <!-- Custom styles for this template -->
+        <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+        <?php wp_head(); ?>
+        <style>
+            .showcase {
+                background: url(<?php echo get_theme_mod('showcase_image', get_bloginfo('template_url').'/img/lapin.jpg'); ?>) no-repeat center center;
+            }
+        </style>
+    </head>
 <body>
 <div class="blog-masthead">
     <div class="container">
         <nav class="blog-nav">
-			<?php
-			wp_nav_menu( array(
-					'menu'              => 'primary',
-					'theme_location'    => 'primary',
-					'depth'             => 2,
-					'container'         => 'div',
-					'container_class'   => 'collapse navbar-collapse',
-					'container_id'      => 'bs-example-navbar-collapse-1',
-					'menu_class'        => 'nav navbar-nav',
-					'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
-					'walker'            => new wp_bootstrap_navwalker())
-			);
-			?>
+            <?php
+            wp_nav_menu(array(
+                    'menu' => 'primary',
+                    'theme_location' => 'primary',
+                    'depth' => 2,
+                    'container' => 'div',
+                    'container_class' => 'collapse navbar-collapse',
+                    'container_id' => 'bs-example-navbar-collapse-1',
+                    'menu_class' => 'nav navbar-nav',
+                    'fallback_cb' => 'wp_bootstrap_navwalker::fallback',
+                    'walker' => new wp_bootstrap_navwalker())
+            );
+            ?>
 
         </nav>
     </div>
 </div>
-    <div class="topnav sticky-top" id="myTopnav">
-        <a href="#home">Hem</a>
-        <a href="http://localhost/examensarbete/product/hasse/">Skapa tavla</a>
-        <a href="http://localhost/examensarbete/illustrationer/">Illustration</a>
-        <a href="http://localhost/examensarbete/om-oss/">Om oss</a>
-        <a href="http://localhost/examensarbete/kontakt/">Kontakt</a>
-        <a href="http://localhost/examensarbete/cart/">Varukorg</a>
-        <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
-    </div>
+<div class="topnav sticky-top" id="myTopnav">
+    <a href="#home">Hem</a>
+    <a href="http://localhost/examensarbete/product/hasse/">Skapa tavla</a>
+    <a href="http://localhost/examensarbete/illustrationer/">Illustration</a>
+    <a href="http://localhost/examensarbete/om-oss/">Om oss</a>
+    <a href="http://localhost/examensarbete/kontakt/">Kontakt</a>
+    <a href="http://localhost/examensarbete/cart/">Varukorg</a>
+    <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
+</div>
 <!--
     HEADER IMAGE
 -->
@@ -65,9 +65,10 @@
 </section>
 <div class="container">
 
-<div class="row">
-    <div class="wrapper">
-        <?php while (have_posts()): the_post(); ?>
+    <div class="row">
+        <div class="wrapper">
+            <?php while (have_posts()):
+            the_post(); ?>
             <!--
            TRUSTBAR SECTION
        -->
@@ -101,7 +102,8 @@
                     <div class="pro1 col-sm-6 col-lg-6">
                         <div class="salediscount">
                             <a href="http://localhost/examensarbete/produkt/hasse/">
-                                <img src=" <?php echo get_bloginfo('template_url') ?>/img/product.jpg" class="images" alt="">
+                                <img src=" <?php echo get_bloginfo('template_url') ?>/img/product.jpg" class="images"
+                                     alt="">
                                 <div class="middle">
                                     <div class="text3">Börja skapa din tavla</div>
                                 </div>
@@ -111,7 +113,8 @@
                     <div class="pro1 col-sm-6 col-lg-6">
                         <div class="salediscount">
                             <a href="http://localhost/examensarbete/produkt/hasse/">
-                                <img src="<?php echo get_bloginfo('template_url') ?>/img/product.jpg" class="images" alt="">
+                                <img src="<?php echo get_bloginfo('template_url') ?>/img/product.jpg" class="images"
+                                     alt="">
                                 <div class="middle">
                                     <div class="text3">Börja skapa din tavla</div>
                                 </div>
@@ -123,7 +126,8 @@
                     <div class="pro2 col-sm-6 col-lg-6">
                         <div class="salediscount">
                             <a href="http://localhost/examensarbete/produkt/hasse/">
-                                <img src="<?php echo get_bloginfo('template_url') ?>/img/product.jpg" class="images" alt="">
+                                <img src="<?php echo get_bloginfo('template_url') ?>/img/product.jpg" class="images"
+                                     alt="">
                                 <div class="middle">
                                     <div class="text3">Börja skapa din tavla</div>
                                 </div>
@@ -133,7 +137,8 @@
                     <div class="pro2 col-sm-6 col-lg-6">
                         <div class="salediscount">
                             <a href="http://localhost/examensarbete/produkt/hasse/">
-                                <img src="<?php echo get_bloginfo('template_url') ?>/img/product.jpg" class="images" alt="">
+                                <img src="<?php echo get_bloginfo('template_url') ?>/img/product.jpg" class="images"
+                                     alt="">
                                 <div class="middle">
                                     <div class="text3">Börja skapa din tavla</div>
                                 </div>
@@ -167,18 +172,18 @@
                     </div>
                 </div>
             </div>
-            <!--
-    TEXT SECTION
-   -->
-            <div class="col-lg-12">
-                <div class="textsection">
-                    <p>* Är du ny kund hos oss, glöm inte att använda din rabatt. Kod: NYKUND   </p>
-                </div>
-            </div>
+        </div>
+    </div>
+</div>
 
-            <!--
-              INSPIRATION SECTION
-            -->
+
+<!--
+  INSPIRATION SECTION
+-->
+<div class="container">
+    <div class="row">
+        <div class="wrapper">
+
             <div class="inspo">
                 <a href="http://localhost/examensarbete/illustrationer/">
                     <img src="<?php echo get_bloginfo('template_url') ?>/img/inspo.png" alt="">
@@ -189,7 +194,7 @@
        -->
 
 
-            <?php if ( is_front_page() ) {  ?>
+            <?php if (is_front_page()) { ?>
                 <div class="instagram"><p>Instagram ,</p></div>
 
                 <?php the_content(); ?>
@@ -197,11 +202,14 @@
             <!--
           NEWSLETTER SECTION
        -->
-            <form action="https://test.us16.list-manage.com/subscribe/post?u=139bb3994f074491a1dc77315&amp;id=0fc232aa0c" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+            <form action="https://test.us16.list-manage.com/subscribe/post?u=139bb3994f074491a1dc77315&amp;id=0fc232aa0c"
+                  method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate"
+                  target="_blank" novalidate>
                 <div class="newsletter col-sm-12">
                     <div class="single">
                         <h1>NYHETSBREV</h1>
-                        <p>Prenumerera på vårat nyhetsbrev och få 10% rabatt på din första beställning. Få löpande erbjudande, insperation och nyheter från äntligen.se.</p>
+                        <p>Prenumerera på vårat nyhetsbrev och få 10% rabatt på din första beställning. Få löpande
+                            erbjudande, insperation och nyheter från äntligen.se.</p>
                         <div class="input-group">
                             <input type="email" class="form-control" placeholder="Din e-post">
                             <span class="input-group-btn">
@@ -214,10 +222,56 @@
                 <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_139bb3994f074491a1dc77315_0fc232aa0c" tabindex="-1" value=""></div>
             </form>
 
-        <?php endwhile;?>
+            <?php endwhile; ?>
+
+        </div>
     </div>
 </div>
+<!--
+START COSTUMER TALK
+-->
+<div class="section-15">
+    <div class="container">
+        <div id="carouselExampleControls111" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="textauthor1 mx-auto col-lg-6">
+                        <i class="fa fa-quote-left" aria-hidden="true"></i>
+
+                        <p><?php the_field('text_1'); ?></p>
+                        <i class="fa fa-quote-right" aria-hidden="true"></i>
+                        <h5><?php the_field('author_1'); ?></h5>
+
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="textauthor2 mx-auto col-lg-6">
+                        <i class="fa fa-quote-left" aria-hidden="true"></i>
+
+                        <p><?php the_field('text_2'); ?></p>
+                        <i class="fa fa-quote-right" aria-hidden="true"></i>
+                        <h5><?php the_field('author_2'); ?></h5>
 
 
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="textauthor3 mx-auto col-lg-6">
+                        <i class="fa fa-quote-left" aria-hidden="true"></i>
+
+                        <p><?php the_field('text_3'); ?></p>
+                        <i class="fa fa-quote-left" aria-hidden="true"></i>
+                        <h5><?php the_field('author_3'); ?></h5>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--
+/END COSTUMER TALK
+-->
 
 <?php get_footer(); ?>
